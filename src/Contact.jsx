670ae8a567c24bff865928905c029dd0a03fcb36
Chenai <186724093+chenai13@users.noreply.github.com>
2026-05-13 12:23:21 +0100

    fixed contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -19,13 +19,14 @@ function Contact() {
         }
         subtitle="Im happy to answer any questions you may have, and I’ll try to respond within a few hours."
       />
-      <section className="-mt-48 min-w-50 pt-12 grid grid-cols-1 md:grid-cols-2 max-w-4xl mx-auto gap-3">
-        <div className="flex h-44 flex-col rounded-lg bg-white shadow-2xl overflow-hidden transition hover:scale-[1.01]">
+      <section className="-mt-48 min-w-50 pt-12 grid grid-cols-1 md:grid-cols-2 mx-auto gap-3">
+         <div className="flex-1">
+        <div className="flex flex-col h-full rounded-lg bg-white shadow-2xl transition hover:scale-[1.01]">
           <div className="flex flex-row items-start border-b border-gray-200 px-8 pt-8 pb-4">
             <div>
               <img
                 src={profile}
-                className="flex h-12 w-12 shrink-0 items-center justify-center rounded-full"
+                className=" h-12 w-12 min-w-12 min-h-12 object-cover rounded-full"
               ></img>
             </div>
 
@@ -36,9 +37,9 @@ function Contact() {
               </h1>
             </div>
           </div>
-          <div className="pt-4 px-10">
+          <div className="pt-4 px-10 pb-6">
             <a href="https://www.linkedin.com/in/chenai-lamarque-090802214/">
-            <button className="hover:ring-1 hover:ring-gray-300 duration-100  text-left text-sm font-medium p-2 border border-gray-300 shadow-sm rounded-xl">
+            <button className="hover:ring-1 hover:ring-gray-300 duration-100 text-left text-sm font-medium p-2 border border-gray-300 shadow-sm rounded-xl">
               Send DM
               
               <div className="inline-flex items-center justify-center ml-2">
@@ -48,20 +49,22 @@ function Contact() {
             </a>
           </div>
         </div>
-        <div className="flex h-44 flex-col rounded-lg bg-white shadow-2xl overflow-hidden transition hover:scale-[1.01]">
+        </div>
+        <div className="flex h-full flex-col rounded-lg bg-white shadow-2xl transition hover:scale-[1.01] overflow-hidden">
+           <div className="flex-1">
           <div className="flex flex-row items-start border-b border-gray-200 px-8 pt-8 pb-4">
             <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-gray-200">
               <IoMailOutline className="text-2xl text-gray-700" />
             </div>
 
-            <div className="flex flex-col pl-5 text-left">
+            <div className="flex flex-col pl-5 text-left min-w-0">
               <p className="text-sm text-gray-500">Send me an email</p>
-              <h1 className="md:text-md text-sm font-semibold text-gray-900 leading-tight ">
+              <h1 className="text-sm font-semibold text-gray-900 leading-tight wrap-break-word">
                 chenailamarque1@gmail.com
               </h1>
             </div>
           </div>
-          <div className="pt-4 px-10">
+          <div className="pt-4 px-10 pb-6">
             <a href="mailto:chenailamarque1@gmail.com">
             <button className="hover:ring-1 hover:ring-gray-300 duration-100 text-left text-sm font-medium p-2 border border-gray-300 shadow-sm rounded-xl">
               Send Email
@@ -72,6 +75,7 @@ function Contact() {
             </button>
              </a>
           </div>
+        </div>
         </div>
       </section>
     </>
